17a15449ab1067c4a88b6d77c56233781e0ee8e2
E-gy <balakhelisey@gmail.com>
2021-05-22 14:47:05 +0200

    📖 documented things!

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # try_all
+
+Rust iterator extensions to operate on `Result`s effectively.
+
+## `try_all`
+
+Tries all items of the iterator until one fails (or all succeed).
+
+**Returns**: The iterator of all successes, or the first failure.
+
+### Examples:
+Useful for propagating failures from within closures with `?` operator
+```
+fn parse_all_numbers(strs: &Vec<&str>) -> Result<Vec<u64>, std::num::ParseIntError> {
+	Ok(strs.iter().map(|s| s.parse()).try_all()?.collect())
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
+//! Rust iterator extensions to operate on `Result`s effectively.
+//!
+//! ## [`try_all`](crate::TryAll::try_all(self))
+//!
+//! Tries all items of the iterator until one fails (or all succeed).
+//!
+//! ### Returns
+//!
+//! The iterator of all successes, or the first failure.
+//! 
+//! ### Examples
+//!
+//! Useful for propagating failures from within closures with `?` operator:
+//! ```
+//! # use crate::try_all::*;
+//! fn parse_all_numbers(strs: &Vec<&str>) -> Result<Vec<u64>, std::num::ParseIntError> {
+//! 	Ok(strs.iter().map(|s| s.parse()).try_all()?.collect())
+//! }
+//! ```
+//!
+
 mod iter;
 pub use iter::*;
